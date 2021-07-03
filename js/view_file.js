b28a861c211693dfb165dc2daecd5839e1302e3c
AngelKode <200angel@live.com.mx>
2021-07-03 00:24:45 -0500

    Avance ver PDF de constancias

---
+++ js/view_file.js
@@ -0,0 +1,17 @@
+const verArchivo = () => {
+    $('#modal_archivo_subido').modal('show');
+}
+
+$(document).ready(() => {
+    //Obtenemos todos los elementos para ver el archivo
+    let view_file = document.getElementsByClassName("view_file");
+    //Convertimos el HTMLCollection a un array
+    let array_view_file = [...view_file];
+
+    //Para cada elemento, agregamos un listener
+    array_view_file.forEach(element => {
+        element.firstElementChild.addEventListener('click',verArchivo,true);
+    });
+});
+
+
